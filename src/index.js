4e988b452512cff927e44a8b4857eaa79ce874cf
Ankita Singhal <AnkitaSinghal@users.noreply.github.com>
2020-07-21 13:47:39 +0200

    learn about the lifecylce methods
    log sequence of lifecycle methods in react

---
+++ src/index.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Home extends React.Component {
+    constructor(props) {
+        console.log('1. constructor')
+        // called first, only once
+        // setup intial values and variables and component state
+        // only place where this.state should be used
+        // everywhere else use setState()
+        super(props);
+        this.state = {
+           mounted: true
+        }
+    }
+
+    static getDerivedStateFromProps(props, state){
+        // used when the state is dependent on props
+        // must returns and object to update state of component
+        // if null is returned no state is changed
+        // it is static and hence no access to this
+        // it is a pure function and no side effects should happen here
+        console.log('2. get derived state from props', props, state);
+
+        if(state.value !== props.value) {
+            return {
+                derivedValue: 'new value',
+                mirrorredProp: 'prop.value'
+            }
+        }
+        return null; //means no change to state
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('2.a should component update', nextProps, nextState);
+        return true;
+        //returning false here makes no chnages to the view
+        // default is true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        // called after render creates the elemets
+        // and before updating the virtual DOM to actual DOM
+        // also know as pre-commit-phase
+        // both current and previous stat and props are available
+        console.log('3.a get snapshot before update', prevProps, prevState);
+        return null;
+        // it is mandatory to retun a value or null
+
+        // if it returns a vluae, it is available as third parameter in component did update
+    }
+
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        // snapshot is the value returned from getSnapShotBeforeUpdate()
+        // called when the DOM is updated
+        console.log('3.b component did update', prevProps, prevState, snapshot)
+    }
+
+    componentWillUnmount(){
+        // last method in lifecycle
+        // all cleanups related to component happen here
+        console.log('5 component will unmount');
+    }
+
+
+    render() {
+        // this is a pure method
+        // mandatory to be present in a component
+
+        console.log('3. render');
+        let newCmp = '<p>this is before mount</p>';
+        if(this.state.mounted) {
+            newCmp = (<p>
+                this is when mounted
+            </p>)
+        }
+        return (
+            <div>
+            <div>
+            {newCmp}
+            </div>
+            <div>
+                <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un)Mount home</button>
+            </div>
+            </div>
+        );
+    }
+    componentDidMount() {
+        // all interactions with the browser DOM and third party integrations
+        // like d3 
+        // all api calls should happen here
+        console.log('4. component did mount')
+    }
+
+    onChangeHomeMounted() {
+        console.log('on change home mounted')
+        this.setState({
+            mounted: !this.state.mounted
+        })
+    }
+
+}
+
+ReactDOM.render(
+    <Home />,
+    document.getElementById('root')
+);
